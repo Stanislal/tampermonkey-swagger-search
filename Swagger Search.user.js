c49d1110efb21a530421ee11fd4b18b63b5cf90f
T35000139 <icaro.baruffi@compasso.com.br>
2020-09-22 17:40:12 -0300

    BT-01 update fix source code

--- Swagger Search.user.js
+++ Swagger Search.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Swagger Search
-// @namespace    https://github.com/Stanislal/tampermonkey-swagger-search
-// @version      1.0
+// @version      1.01
 // @description  Filter Swagger by name.
 // @author       Icaro Batistella Baruffi  <icru3@hotmail.com>
 // @match        http://swaggerui.pontoslivelo.com.br/*
+// @source       https://github.com/Stanislal/tampermonkey-swagger-search
 // @grant        none
 // @require http://code.jquery.com/jquery-3.3.1.min.js
 // @require https://raw.githubusercontent.com/Stanislal/tampermonkey-swagger-search/master/SwaggerSearch.js
